3366b0317765212cb5e6fa3e4c750b6f6df28214
Kaitlyn Middlebrook <kmiddlebrook2000@gmail.com>
2018-11-06 13:52:19 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -14,5 +14,9 @@
 phrase = "Where are the cookies?"
 
 if phrase.downcase
-  puts ""
+  puts "HUH?! SPEAK UP SONNY!"
+elsif phrase.upcase
+  puts "NO, NOT SINCE 1938!"
+elsif phrase == "I LOVE YOU GRANDMA!"
+  puts 
 end  
