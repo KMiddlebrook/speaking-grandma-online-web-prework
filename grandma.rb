c5ce14969530c58ee7b7ba407bb7d21a3488b8f1
Kaitlyn Middlebrook <kmiddlebrook2000@gmail.com>
2018-11-06 14:04:10 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -3,9 +3,12 @@ def speak_to_grandma(phrase)
   
   if phrase.upcase
     puts "NO, NOT SINCE 1938!"
+    phrase.upcase
   elsif phrase == "I LOVE YOU GRANDMA!"
     puts "I LOVE YOU TOO PUMPKIN!"
+    phrase == "I LOVE YOU GRANDMA!"
   else
     puts "HUH?! SPEAK UP SONNY!"
+    phrase.downcase
   end
 end
