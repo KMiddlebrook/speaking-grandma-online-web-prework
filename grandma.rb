693a9e00dc0265a26da786102b1a733cb1486081
Kaitlyn Middlebrook <kmiddlebrook2000@gmail.com>
2018-11-06 14:02:06 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,9 +1,10 @@
 phrase = "How are you, grandma?"
-
-if phrase.upcase
-  puts "NO, NOT SINCE 1938!"
-elsif phrase == "I LOVE YOU GRANDMA!"
-  puts "I LOVE YOU TOO PUMPKIN!"
-else
-  puts "HUH?! SPEAK UP SONNY!"
-end  
+def speak_to_grandma
+  if phrase.upcase
+    puts "NO, NOT SINCE 1938!"
+  elsif phrase == "I LOVE YOU GRANDMA!"
+    puts "I LOVE YOU TOO PUMPKIN!"
+  else
+    puts "HUH?! SPEAK UP SONNY!"
+  end
+end
